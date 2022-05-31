d5987cc8050d60547da32db8754ab59c8455f275
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2022-05-30 22:25:46 -0300

    Code Wars

--- Python/5kyu_Scramblies.py
+++ Python/5kyu_Scramblies.py
@@ -1,6 +1,5 @@
 # * Problem Statement * #
 
-
 # Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
 #
 # Notes:
--- Python/6kyu_PlayingWithDigits.py
+++ Python/6kyu_PlayingWithDigits.py
@@ -1,11 +1,8 @@
 # * Problem Statement * #
 
 # Some numbers have funny properties. For example:
-#
 # 89 --> 8¹ + 9² = 89 * 1
-#
 # 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
-#
 # 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 #
 # Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
