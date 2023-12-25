401b1f5063471bcd3b1eb3709f34310493cb20d1
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-25 19:15:56 -0300

    Moving zeros to the end

--- Python/Finished/5-kyu/5kyu_MovingZerosToTheEnd.py
+++
@@ -1,6 +0,0 @@
-def move_zeros(array):
-    for i in array:
-        if i == 0:
-            array.append(array.pop(array.index(0)))
-
-    return array
---
+++ Python/Finished/5-kyu/Moving_Zeros_To_The_End.py
@@ -0,0 +1,12 @@
+"""
+    Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+    move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+
+def move_zeros(array):
+    for i in array:
+        if i == 0:
+            array.append(array.pop(array.index(0)))
+
+    return array
