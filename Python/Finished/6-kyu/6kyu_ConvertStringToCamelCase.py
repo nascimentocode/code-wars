a21f8e6bbbedc62a9f1ba739c8a99637ddd34822
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-25 20:17:09 -0300

    Convert string to camel case

--- Python/Finished/6-kyu/6kyu_ConvertStringToCamelCase.py
+++
@@ -1,6 +0,0 @@
-def to_camel_case(text):
-    removed = text.replace('-', ' ').replace('_', ' ').split()
-    if len(removed) == 0:
-        return ''
-
-    return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
