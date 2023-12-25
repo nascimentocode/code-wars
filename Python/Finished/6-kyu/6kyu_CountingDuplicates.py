ea7e2394941b261624fd4c4ddaaf8646667db1d6
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-25 20:16:55 -0300

    Counting duplicates

--- Python/Finished/6-kyu/6kyu_CountingDuplicates.py
+++
@@ -1,9 +0,0 @@
-def duplicate_count(text):
-    lowerText = text.lower()
-    result = []
-
-    for i in lowerText:
-        if lowerText.count(i) > 1: # Ou poderia ser assim → if lowerText.count(i) > 1 and i not in result:
-            result.append(i)
-    
-    return len(set(result)) # Nem precisaria mais do set(result)
