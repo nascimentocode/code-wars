b47f38acad0d05bbbb92e4fb8e5575004f7f3b7a
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-25 19:14:44 -0300

    Simple pig latin

--- Python/Finished/5-kyu/5kyu_SimplePigLatin.py
+++
@@ -1,9 +0,0 @@
-def pig_it(text):
-    newText = []
-    for word in text.split():
-        if word.isalpha():
-            newText.append(word[1:]+word[0]+'ay')
-        else:
-            newText.append(word)
-        
-    return ' '.join(newText)
