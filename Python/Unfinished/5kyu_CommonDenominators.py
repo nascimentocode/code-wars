9c72abfeb56f8a72b4ab3ed6b9e539a77c5b52e3
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-02-04 05:00:51 -0300

    Codewars katas

--- Python/Unfinished/5kyu_CommonDenominators.py
+++
@@ -1,7 +0,0 @@
-def convert_fracts(lst):
-    for i in lst:
-        lst[lst.index(i)] = [12//i[1], 12]
-        
-    return lst
-
-print(convert_fracts([[1, 2], [1, 3], [1, 4]]))
--- Python/Unfinished/5kyu_JosephusSurvivor.py
+++
@@ -1,8 +0,0 @@
-def josephus_survivor(n,k):
-    list = []
-    for i in range(1, n+1):
-        list.append(i)
-
-    return list   
-
-print(josephus_survivor(7,3))
